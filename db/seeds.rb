e070f5dde7f7405fd4e88aa484190ae6fcb0bf3c
scrabill <shannon.crabill@gmail.com>
2019-10-20 14:42:54 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "Themes that I love" , description: "It's been a long road...")
